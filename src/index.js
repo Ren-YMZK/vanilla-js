ce4f24f35c56ad2fff4317cc2f589af4811a8271
Ren Yamazaki <r26763041@gmail.com>
2025-06-24 03:46:36 +0900

    関数について

--- src/index.js
+++ src/index.js
@@ -46,3 +46,26 @@
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
+// function func1(str) {
+//   return str;
+// }
+
+// const func1 = function (str) {
+//   return str;
+// };
+
+// console.log(func1("func1です。"));
+
+// const func2 = (str) => {
+//   return str;
+// };
+
+// const func2 = (str) => str;
+
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
