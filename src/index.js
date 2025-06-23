ea683806931a0c8b2a3f3f86e7127aeea45b66a0
Ren Yamazaki <r26763041@gmail.com>
2025-06-24 03:46:14 +0900

    変数について

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vanilla!</h1>
-`;
+// val1 = "val1を上書き";
+// console.log(val1);
 
-setupCounter(document.querySelector("#counter"));
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const val4 = {
+//   name: "れん",
+//   age: 23,
+// };
+
+// val4.name = "ren";
+// val4.address = "Tokyo";
+
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey")
+// console.log(val5);
+
+// const name = "れん";
+// const age = 33;
+
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
