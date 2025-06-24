72861007f04f633f05ff50f857f2fdf2d22b6d65
Ren Yamazaki <r26763041@gmail.com>
2025-06-25 00:33:19 +0900

    分割代入について

--- src/index.js
+++ src/index.js
@@ -69,3 +69,24 @@
 // };
 
 // console.log(func3(10, 20));
+
+// const myProfile = {
+//   name: "れん",
+//   age: 23,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["れん", 23];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
